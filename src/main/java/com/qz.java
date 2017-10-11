e659eac1e0ef5fff29c208058c98558b04dafad3
ginoy <GinoyJda>
2017-10-11 10:39:05 +0800

    test file

---
+++ src/main/java/com/qz.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 17-10-11
+ * Time: 上午10:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class qz {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
